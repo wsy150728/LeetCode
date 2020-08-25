e6ce82d5bb0af893e349e6d70d2cce2110a9a4cd
wangshengyi <wangshengyi@qutoutiao.net>
2020-08-26 00:30:16 +0800

    q976

--- java/code/q976/LargestPerimeter.java
+++ java/code/q976/LargestPerimeter.java
@@ -8,16 +8,14 @@ import java.util.Arrays;
  */
 public class LargestPerimeter {
     public static int largestPerimeter(int[] A) {
-        int sum = 0;
         Arrays.sort(A);
         for (int i = A.length - 1;i > 1; i--){
             if(A[i-2] + A[i-1] > A[i]){
-                sum = A[i] + A[i-1] + A[i-2];
-                break;
+                return A[i] + A[i-1] + A[i-2];
             }
         }
 
-        return sum;
+        return 0;
     }
 
 
