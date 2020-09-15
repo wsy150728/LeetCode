cd2d2b9777160e4ebde9c1d15efed2eeac421972
wangshengyi <wangshengyi@qutoutiao.net>
2020-09-15 18:31:45 +0800

    q496

---
+++ java/code/q496/NextGreaterElement.java
@@ -0,0 +1,61 @@
+package code.q496;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Tag Stack
+ */
+public class NextGreaterElement {
+
+    /**
+     * 数组的方法
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i< nums2.length; i++) {
+            map.put(nums2[i],i);
+        }
+        for (int x = 0; x < nums1.length;x++){
+            res[x] = -1;
+            for (int y = map.get(nums1[x]) + 1; y < nums2.length;y++){
+                if(nums1[x] < nums2[y]){
+                    res[x] = nums2[y];
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * 栈的方法
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i< nums2.length; i++) {
+            map.put(nums2[i],i);
+        }
+        for (int x = 0; x < nums1.length;x++){
+            res[x] = -1;
+            for (int y = map.get(nums1[x]) + 1; y < nums2.length;y++){
+                if(nums1[x] < nums2[y]){
+                    res[x] = nums2[y];
+                    break;
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ java/test/q496/NextGreaterElementTest.java
@@ -0,0 +1,18 @@
+package test.q496;
+
+import code.q496.NextGreaterElement;
+
+import java.util.Arrays;
+
+public class NextGreaterElementTest {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        NextGreaterElement model = new NextGreaterElement();
+
+        int[] res = model.nextGreaterElement(nums1, nums2);
+
+        System.out.println(Arrays.toString(res));
+    }
+}
