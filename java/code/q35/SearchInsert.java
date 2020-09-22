a19ec3a87775d497110da94778152356e6afe5b6
wangshengyi <wangshengyi@qutoutiao.net>
2020-09-22 14:07:02 +0800

    change

--- java/code/q35/SearchInsert.java
+++ java/code/q35/SearchInsert.java
@@ -30,19 +30,19 @@ public class SearchInsert {
     public int searchInsert2(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int ans = nums.length;
 
         while (left <= right){
             int mid = left + (right - left) / 2;
-            if(nums[mid] >= target){
-                ans = mid;
+            if(nums[mid] > target){
                 right = mid - 1;
-            }else {
+            }else if(nums[mid] < target) {
                 left = mid + 1;
+            }else{
+                return mid;
             }
         }
 
-        return ans;
+        return nums.length;
     }
 
 }
